87eb54f417e9f846d2466c4fe544ccc3db02a2d7
TelRich <95597275+TelRich@users.noreply.github.com>
2022-03-23 19:51:12 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,68 +1,47 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * print_buffer - function that prints a buffer
- * @b: character pointer
- * @size: integer value
+ * print_buffer - Print the entire buffer w/ certain conditions
+ * @b: The buffer to print
+ * @size: The size of the buffer
  */
-
 void print_buffer(char *b, int size)
 {
-	int i;
-	int count = 0;
-	char *temp = b;
-	int j = 0;
-	int k;
-	int l;
-	static int val1;
-	static int val2;
-	int space = 1;
+	int i, j;
 
-	if (size < 0)
+	i = 0;
+	if (size <= 0)
+		putchar('\n');
+	else
 	{
-		printf("\n");
-	}
-	val1 = 0;
-	val2 = 0;
-	for (i = 0; i <= size && count <= 10; i++)
-	{
-		temp[j] = b[i];
-		if (count == 0)
+		while (i < size)
 		{
-			printf("0%8p: ", &b[i]);
-		}
-		++count;
-		if (count == 10)
-		{
-			for (l = 0; l < count; l++)
+			printf("%08x: ", i);
+			j = 0;
+			while (j < 10)
 			{
-				printf("%02x", temp[val1]);
-				if (space % 2 == 0)
+				if (j % 2 == 0 && j > 0)
 					printf(" ");
-				space++;
-				val1++;
+				if (j + i > size - 1)
+					printf("  ");
+				else
+					printf("%.2x", b[j + i]);
+				j++;
 			}
-			for (k = 0; k < count; k++)
+			putchar(' ');
+			j = 0;
+			while (j < 10)
 			{
-				printf("%c", temp[val2]);
-				if (temp[val2] == '\\' || temp[val2] == '\0' ||
-						temp[val2] == '\1' ||
-						temp[val2] == '\2' ||
-						temp[val2] == '\3' ||
-						temp[val2] == '\4' ||
-						temp[val2] == '\5' ||
-						temp[val2] == '\6' ||
-						temp[val2] == '\7' ||
-						temp[val2] == '\n')
-					printf(".");
-				val2++;
+				if (j + i > size - 1)
+					break;
+				if (b[j + i] >= ' ' && b[j + i] <= '~')
+					putchar(b[j + i]);
+				else
+					putchar('.');
+				j++;
 			}
-			count = 0;
-			space = 1;
-			printf("\n");
+			putchar('\n');
+			i += 10;
 		}
-		j++;
 	}
-	printf("\n");
 }
